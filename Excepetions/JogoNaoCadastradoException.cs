5556b671aba0f4f74ede5eb2b7bb02a25e414b05
Matheus Yuri <matheusyurirs@gmail.com>
2021-10-16 20:00:32 -0300

    Api De Catalogo de jogos ESTUDO

---
+++ Excepetions/JogoNaoCadastradoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCatalogo.Excepetions
+{
+    public class JogoNaoCadastradoException : Exception
+    {
+        public JogoNaoCadastradoException()
+         : base("Este jogo não está cadastrado")
+        { }
+    }
+}
